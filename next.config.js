e79f3ef3014e417f58874fae10e19f8c67c3ff28
AparinAA <alxaparin@gmail.com>
2022-08-29 18:47:46 +0300

    add next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const images = {
   dangerouslyAllowSVG: true,
   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 }
+
 module.exports = { 
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   nextConfig,
   images
 }
