eaceef79233667f3932ae579333069d746547f75
AparinAA <alxaparin@gmail.com>
2022-08-31 18:11:35 +0300

    add opacity threshold

--- public/js.js
+++ public/js.js
@@ -1,47 +1,24 @@
-const height = ( (document.documentElement.clientHeight - 50) / 3 ) + 'px';
+const height = ( (document.documentElement.clientHeight - 50) / 5 ) + 'px';
+
 let options = {
     root: null,
-    rootMargin: height,
+    rootMargin: 0,
     threshold: new Array(50).fill(0).map( (item, index) => (index + 1) * 0.02)
-}
+},
+observer = new IntersectionObserver(cleaned, options),
+info = document.getElementById('info'),
+curPos = 1;
 
-
-let observer = new IntersectionObserver(cleaned, options);
-document.getElementById('title').style.opacity = 1;
 document.scrollingElement.scrollTop = 0;
-
-let info = document.getElementById('info');
+//document.getElementById('title').style.opacity = 1;
 info.style.opacity = 0;
 
-let curPos = 1;
-
-function cleaned(entries, observer) {
+function cleaned(entries) {
     const [entry] = entries;
-    
+    console.info(entry.intersectionRatio)
     if ( (Number(entry.target.style.opacity) <= 1.0) && (Number(entry.target.style.opacity) >= 0) ) {
-        if ( (curPos > Number(entry.intersectionRatio) ) ) {
-            info.style.opacity = Number(info.style.opacity) + 0.05;
-            entry.target.style.opacity = Number(entry.target.style.opacity) - 0.05;
-            
-            if ( Number(entry.target.style.opacity) < 0) {
-                entry.target.style.opacity = 0;
-            } 
-            
-            if ( Number(info.style.opacity) > 1) {
-                info.style.opacity = 1;
-            }
-        } else if ( ( curPos < Number(entry.intersectionRatio) ) && (Number(entry.intersectionRatio) > 0.3) ) {
-            info.style.opacity = Number(info.style.opacity) - 0.05;
-            entry.target.style.opacity = Number(entry.target.style.opacity) + 0.05;
-            
-            if (  Number(entry.target.style.opacity) > 1) {
-                entry.target.style.opacity = 1;
-            } 
-            
-            if (  Number(info.style.opacity) < 0) {
-                info.style.opacity = 0;
-            }
-        }
+        info.style.opacity = 1 - Number(entry.intersectionRatio);
+        entry.target.style.opacity = Number(entry.intersectionRatio) * Number(entry.intersectionRatio) * Number(entry.intersectionRatio);
     }
     curPos = Number(entry.intersectionRatio);
 }
