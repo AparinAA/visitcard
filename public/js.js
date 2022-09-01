7d4d7229b7a98d3a576d60c69f7c3004231d8e50
AparinAA <alxaparin@gmail.com>
2022-09-01 09:53:35 +0300

    add opacity threshold

--- public/js.js
+++ public/js.js
@@ -1,26 +1,21 @@
-const height = ( (document.documentElement.clientHeight - 50) / 5 ) + 'px';
-
-let options = {
-    root: null,
-    rootMargin: 0,
-    threshold: new Array(50).fill(0).map( (item, index) => (index + 1) * 0.02)
-},
-observer = new IntersectionObserver(cleaned, options),
-info = document.getElementById('info'),
-curPos = 1;
+const height = ( (document.documentElement.clientHeight - 50) / 5 ) + 'px',
+      options = {
+          root: null,
+          rootMargin: 0,
+          threshold: new Array(50).fill(0).map( (item, index) => (index + 1) * 0.02)
+      },
+      observer = new IntersectionObserver(opacityControl, options),
+      info = document.getElementById('info');
 
 document.scrollingElement.scrollTop = 0;
-document.getElementById('title').style.opacity = 1;
 info.style.opacity = 0;
 
-function cleaned(entries) {
+function opacityControl(entries) {
     const [entry] = entries;
-    console.info(entry.intersectionRatio)
-    if ( (Number(entry.target.style.opacity) <= 1.0) && (Number(entry.target.style.opacity) >= 0) ) {
-        info.style.opacity = 1 - Number(entry.intersectionRatio);
-        entry.target.style.opacity = Number(entry.intersectionRatio) * Number(entry.intersectionRatio) * Number(entry.intersectionRatio) * Number(entry.intersectionRatio);
-    }
-    curPos = Number(entry.intersectionRatio);
+    const ratio = Number(entry.intersectionRatio);
+
+    info.style.opacity = 1 - ratio;
+    entry.target.style.opacity = ratio * ratio * ratio * ratio;
 }
 
 observer.observe(document.getElementById('title'));
