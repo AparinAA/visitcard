9cd17c8780120dbcda08c07013c133e670a6a0fe
AparinAA <alxaparin@gmail.com>
2022-09-12 16:56:24 +0300

    refactoring code

--- data/read.js
+++ data/read.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 
 export function readData(prefix) {
-    const pathDirectory = path.join(prefix, 'cardItems.json');
+    const pathDirectory = path.join('','data/cardItems.json');
     const data = JSON.parse(fs.readFileSync(pathDirectory, {"encoding": "utf-8"})); 
     return data;
 }
