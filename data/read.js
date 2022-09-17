5191c5824f2d577cbaa247b01a13aaf5747f2ed4
AparinAA <alxaparin@gmail.com>
2022-09-17 16:24:10 +0300

    sort

--- data/read.js
+++ data/read.js
@@ -5,6 +5,6 @@ export function readData(prefix) {
     const pathDirectory = path.join('', 'data/cardItems.json');
     const listCard = JSON.parse(fs.readFileSync(pathDirectory, {"encoding": "utf-8"}));
 
-    listCard.sort( (a, b) => a.id - b.id < 0);
+    listCard.sort( (a, b) => a.id - b.id);
     return listCard;
 }
