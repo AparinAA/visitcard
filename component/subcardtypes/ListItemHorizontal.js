87705f1da84bdab8c9538af951bb8bd793398258
AparinAA <alxaparin@gmail.com>
2022-12-02 00:35:43 +0300

    add experience

--- component/subcardtypes/ListItemHorizontal.js
+++ component/subcardtypes/ListItemHorizontal.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import styles from '../../styles/Home.module.css';
+import Image from 'next/image';
+
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 function ListItemHorizontal(props) {
     const { item } = props;
     const [ name = null, exp = 0 ] = [Object.keys(item)[0], Object.values(item)[0]];
     const lvl = [];
 
-    for ( let i = 0; i < exp; i++ ) {
-        lvl.push(<span key={i} style={{backgroudColor: "green !importan", width: "max-content", margin: 0}}>-</span>)
+    for ( let i = 0; i < Math.min(exp,5); i++ ) {
+        lvl.push(<div className={styles.bar} key={i}><Image src={`${prefix}/bar.png`} width={15} height={10}/></div>)
     }
     return (
         <li>
-            
+            <div className={styles.listHorNameItem}>
                 <ReactMarkdown components={{p: React.Fragment}}>
                     {name}
                 </ReactMarkdown>
+            </div>
+            <div className={styles.progressbar}>
+                {lvl}
+                <span>{exp > 5 ? '5+ experience' : `${exp} experience`}</span>
+            </div>
             
         </li>
     );
