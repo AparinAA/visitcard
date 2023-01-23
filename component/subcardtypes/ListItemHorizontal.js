543a8ead8c4328261452840c9cdf602df7131f6f
AparinAA <alxaparin@gmail.com>
2023-01-23 15:44:09 +0300

    fixed UI text experience if exp < 1 years

--- component/subcardtypes/ListItemHorizontal.js
+++ component/subcardtypes/ListItemHorizontal.js
@@ -5,26 +5,38 @@ import Image from 'next/image';
 
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
+const textExp = (exp) => {
+    if (exp > 5) {
+        return '> 5 years';
+    } else if (exp === 0) {
+        return '< 1 year';
+    } else {
+        return exp > 1 ? `${exp} years` : `${exp} year`;
+    }
+}
+
 function ListItemHorizontal(props) {
     const { item } = props;
-    const [ name = null, exp = 0 ] = [Object.keys(item)[0], Object.values(item)[0]];
+    const [name = null, exp = 0] = [Object.keys(item)[0], Object.values(item)[0]];
     const lvl = [];
 
-    for ( let i = 0; i < Math.min(exp,5); i++ ) {
-        lvl.push(<div className={styles.bar} key={i}><Image src={`${prefix}/bar.png`} width={15} height={10}/></div>)
+    for (let i = 0; i < Math.min(Math.max(exp, 1), 5); i++) {
+        lvl.push(<div className={styles.bar} key={i}><Image src={`${prefix}/bar.png`} width={15} height={10} alt="" /></div>)
     }
+
+    const txtExp = textExp(exp);
     return (
         <li>
             <div className={styles.listHorNameItem}>
-                <ReactMarkdown components={{p: React.Fragment}}>
+                <ReactMarkdown components={{ p: React.Fragment }}>
                     {name}
                 </ReactMarkdown>
             </div>
             <div className={styles.progressbar}>
                 {lvl}
-                <span>{exp > 5 ? '5+ years' : (exp > 1 ? `${exp} years` : `${exp} year`)}</span>
+                <span>{txtExp}</span>
             </div>
-            
+
         </li>
     );
 }
