78eb729c848ee2add39453fb636d7f85e46a5ac4
AparinAA <alxaparin@gmail.com>
2023-07-28 10:10:08 +0200

    add og

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,49 @@
-import { useInView } from 'react-intersection-observer';
-import React, { useEffect, useReducer, useState } from 'react';
+import { useInView } from "react-intersection-observer";
+import React, { useEffect, useReducer, useState } from "react";
 
-import Head from 'next/head';
+import Head from "next/head";
 //import Script from 'next/script';
-import styles from '../styles/Home.module.css';
-import Card from '../component/card';
-import SubcardCard from '../component/subcard';
-import ChangeLang from '../component/ChangeLang';
+import styles from "../styles/Home.module.css";
+import Card from "../component/card";
+import SubcardCard from "../component/subcard";
+import ChangeLang from "../component/ChangeLang";
 
-import { readData } from '../data/read';
+import { readData } from "../data/read";
 
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 function reducer(state, action) {
     const lang = action;
-    if (/ru/ig.test(lang)) {
+    if (/ru/gi.test(lang)) {
         return "RU";
     }
 
-    return "EN";;
+    return "EN";
 }
+
+export const metadata = {
+    openGraph: {
+        title: "Aparin Aleksandr",
+        description: "The CV page",
+        url: "https://www.aleksandraparin.site",
+        siteName: "Visitcard",
+        images: [
+            {
+                url: "https://nextjs.org/og.png",
+                width: 800,
+                height: 600,
+            },
+            {
+                url: "https://nextjs.org/og-alt.png",
+                width: 1800,
+                height: 1600,
+                alt: "My custom alt",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+};
 
 function Home({ data }) {
     const num = 100;
@@ -32,40 +56,43 @@ function Home({ data }) {
     const [ref, inView, entry] = useInView({
         /* Optional options */
         root: null,
-        rootMargin: '100px',
-        threshold: [...Array(num).keys()].map(index => (index + 1) * rate),
+        rootMargin: "100px",
+        threshold: [...Array(num).keys()].map((index) => (index + 1) * rate),
     });
 
     const [lang, setLang] = useReducer(reducer, "EN");
     const listCard = data[lang];
     const ratio = entry?.intersectionRatio ?? 1;
 
-    const typeGrid = 'info'//'mainGrid';
+    const typeGrid = "info"; //'mainGrid';
 
-    const gridCards =
+    const gridCards = (
         <div className={styles[typeGrid]} id="info">
-            {listCard?.map(item => {
-                item.info.sort((a, b) => a?.id && b?.id ? a?.id - b?.id : 1);
-                return <Card
-                    title={item?.title}
-                    key={`card_${item.id}`}
-                    isSticky={item?.id === 1}
-                >
-                    {item.info.map(infItem => {
-                        return <SubcardCard
-                            key={`subcard_${item.id}_${infItem?.id}`}
-                            title={infItem?.title}
-                            description={infItem?.description}
-                            subdescription={infItem?.subdescription}
-                            body={infItem?.child}
-                        />
-                    })}
-                    {item?.child}
-                </Card>
-            }
-
-            )}
+            {listCard?.map((item) => {
+                item.info.sort((a, b) => (a?.id && b?.id ? a?.id - b?.id : 1));
+                return (
+                    <Card
+                        title={item?.title}
+                        key={`card_${item.id}`}
+                        isSticky={item?.id === 1}
+                    >
+                        {item.info.map((infItem) => {
+                            return (
+                                <SubcardCard
+                                    key={`subcard_${item.id}_${infItem?.id}`}
+                                    title={infItem?.title}
+                                    description={infItem?.description}
+                                    subdescription={infItem?.subdescription}
+                                    body={infItem?.child}
+                                />
+                            );
+                        })}
+                        {item?.child}
+                    </Card>
+                );
+            })}
         </div>
+    );
 
     return (
         <div className={styles.container} id="body">
@@ -79,20 +106,34 @@ function Home({ data }) {
                 className={styles.title}
                 id="title"
                 ref={ref}
-                style={{ 'opacity': ratio * ratio * ratio * ratio * ratio * ratio * ratio * ratio - 0.1, backgroundImage: `url(${prefix}/welcomtextMini.svg)` }}
+                style={{
+                    opacity:
+                        ratio *
+                            ratio *
+                            ratio *
+                            ratio *
+                            ratio *
+                            ratio *
+                            ratio *
+                            ratio -
+                        0.1,
+                    backgroundImage: `url(${prefix}/welcomtextMini.svg)`,
+                }}
             />
 
-            <main className={styles.main} id="main" style={{ "opacity": 1 - 1.2 * ratio * ratio }}>
-
+            <main
+                className={styles.main}
+                id="main"
+                style={{ opacity: 1 - 1.2 * ratio * ratio }}
+            >
                 <ChangeLang props={{ prefix, inView, setLang, lang }} />
                 {gridCards}
                 <footer className={styles.footer}>
-                    Powered by{' '} Aparin Aleksandr
+                    Powered by Aparin Aleksandr
                 </footer>
             </main>
-
-        </div >
-    )
+        </div>
+    );
 }
 
 export default React.memo(Home);
@@ -100,6 +141,6 @@ export default React.memo(Home);
 export async function getStaticProps() {
     const data = readData(prefix);
     return {
-        props: { data }
-    }
+        props: { data },
+    };
 }
