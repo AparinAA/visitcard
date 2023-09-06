4cfa1a19d6bda86470c8ea053fe017592f1e77a8
Александр Апарин <aparinalx@yandex.ru>
2023-09-06 14:47:26 +0200

    update cv

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-}
+    reactStrictMode: true,
+};
 const images = {
-  dangerouslyAllowSVG: true,
-  contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  domains: ['leetcode.com', 'assets.leetcode.com'],
-  minimumCacheTTL: 31536000
-}
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ["leetcode.com", "assets.leetcode.com"],
+    minimumCacheTTL: 31536000,
+};
 
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  nextConfig,
-  images
-}
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    nextConfig,
+    images,
+};
