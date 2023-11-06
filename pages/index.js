c6c1ff4f2336b5307700ce2e7502ec680c0b6f74
Александр Апарин <aparinalx@yandex.ru>
2023-11-06 18:47:21 +0100

    change url

--- pages/index.js
+++ pages/index.js
@@ -13,134 +13,134 @@ import { readData } from "../data/read";
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 function reducer(state, action) {
-    const lang = action;
-    if (/ru/gi.test(lang)) {
-        return "RU";
-    }
+	const lang = action;
+	if (/ru/gi.test(lang)) {
+		return "RU";
+	}
 
-    return "EN";
+	return "EN";
 }
 
 export const metadata = {
-    openGraph: {
-        title: "Aparin Aleksandr",
-        description: "The CV page",
-        url: "https://www.aleksandraparin.site",
-        siteName: "Visitcard",
-        images: [
-            {
-                url: "https://nextjs.org/og.png",
-                width: 800,
-                height: 600,
-            },
-            {
-                url: "https://nextjs.org/og-alt.png",
-                width: 1800,
-                height: 1600,
-                alt: "My custom alt",
-            },
-        ],
-        locale: "en_US",
-        type: "website",
-    },
+	openGraph: {
+		title: "Aparin Aleksandr",
+		description: "The CV page",
+		url: "https://aparinaleksandr.site",
+		siteName: "Visitcard",
+		images: [
+			{
+				url: "https://nextjs.org/og.png",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "https://nextjs.org/og-alt.png",
+				width: 1800,
+				height: 1600,
+				alt: "My custom alt",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 function Home({ data }) {
-    const num = 100;
-    const rate = 1 / (num + 1);
+	const num = 100;
+	const rate = 1 / (num + 1);
 
-    useEffect(() => {
-        setLang(window.navigator.language);
-    }, []);
+	useEffect(() => {
+		setLang(window.navigator.language);
+	}, []);
 
-    const [ref, inView, entry] = useInView({
-        /* Optional options */
-        root: null,
-        rootMargin: "100px",
-        threshold: [...Array(num).keys()].map((index) => (index + 1) * rate),
-    });
+	const [ref, inView, entry] = useInView({
+		/* Optional options */
+		root: null,
+		rootMargin: "100px",
+		threshold: [...Array(num).keys()].map((index) => (index + 1) * rate),
+	});
 
-    const [lang, setLang] = useReducer(reducer, "EN");
-    const listCard = data[lang];
-    const ratio = entry?.intersectionRatio ?? 1;
+	const [lang, setLang] = useReducer(reducer, "EN");
+	const listCard = data[lang];
+	const ratio = entry?.intersectionRatio ?? 1;
 
-    const typeGrid = "info"; //'mainGrid';
+	const typeGrid = "info"; //'mainGrid';
 
-    const gridCards = (
-        <div className={styles[typeGrid]} id="info">
-            {listCard?.map((item) => {
-                item.info.sort((a, b) => (a?.id && b?.id ? a?.id - b?.id : 1));
-                return (
-                    <Card
-                        title={item?.title}
-                        key={`card_${item.id}`}
-                        isSticky={item?.id === 1}
-                    >
-                        {item.info.map((infItem) => {
-                            return (
-                                <SubcardCard
-                                    key={`subcard_${item.id}_${infItem?.id}`}
-                                    title={infItem?.title}
-                                    description={infItem?.description}
-                                    subdescription={infItem?.subdescription}
-                                    body={infItem?.child}
-                                />
-                            );
-                        })}
-                        {item?.child}
-                    </Card>
-                );
-            })}
-        </div>
-    );
+	const gridCards = (
+		<div className={styles[typeGrid]} id="info">
+			{listCard?.map((item) => {
+				item.info.sort((a, b) => (a?.id && b?.id ? a?.id - b?.id : 1));
+				return (
+					<Card
+						title={item?.title}
+						key={`card_${item.id}`}
+						isSticky={item?.id === 1}
+					>
+						{item.info.map((infItem) => {
+							return (
+								<SubcardCard
+									key={`subcard_${item.id}_${infItem?.id}`}
+									title={infItem?.title}
+									description={infItem?.description}
+									subdescription={infItem?.subdescription}
+									body={infItem?.child}
+								/>
+							);
+						})}
+						{item?.child}
+					</Card>
+				);
+			})}
+		</div>
+	);
 
-    return (
-        <div className={styles.container} id="body">
-            <Head>
-                <title>Visit card</title>
-                <meta name="description" content="" />
-                <link rel="icon" href={`${prefix}/aa.ico`} />
-            </Head>
+	return (
+		<div className={styles.container} id="body">
+			<Head>
+				<title>Visit card</title>
+				<meta name="description" content="" />
+				<link rel="icon" href={`${prefix}/aa.ico`} />
+			</Head>
 
-            <div
-                className={styles.title}
-                id="title"
-                ref={ref}
-                style={{
-                    opacity:
-                        ratio *
-                            ratio *
-                            ratio *
-                            ratio *
-                            ratio *
-                            ratio *
-                            ratio *
-                            ratio -
-                        0.1,
-                    backgroundImage: `url(${prefix}/welcomtextMini.svg)`,
-                }}
-            />
+			<div
+				className={styles.title}
+				id="title"
+				ref={ref}
+				style={{
+					opacity:
+						ratio *
+							ratio *
+							ratio *
+							ratio *
+							ratio *
+							ratio *
+							ratio *
+							ratio -
+						0.1,
+					backgroundImage: `url(${prefix}/welcomtextMini.svg)`,
+				}}
+			/>
 
-            <main
-                className={styles.main}
-                id="main"
-                style={{ opacity: 1 - 1.2 * ratio * ratio }}
-            >
-                <ChangeLang props={{ prefix, inView, setLang, lang }} />
-                {gridCards}
-                <footer className={styles.footer}>
-                    Powered by Aparin Aleksandr
-                </footer>
-            </main>
-        </div>
-    );
+			<main
+				className={styles.main}
+				id="main"
+				style={{ opacity: 1 - 1.2 * ratio * ratio }}
+			>
+				<ChangeLang props={{ prefix, inView, setLang, lang }} />
+				{gridCards}
+				<footer className={styles.footer}>
+					Powered by Aparin Aleksandr
+				</footer>
+			</main>
+		</div>
+	);
 }
 
 export default React.memo(Home);
 
 export async function getStaticProps() {
-    const data = readData(prefix);
-    return {
-        props: { data },
-    };
+	const data = readData(prefix);
+	return {
+		props: { data },
+	};
 }
