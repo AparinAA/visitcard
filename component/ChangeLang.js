3fc4f1962ff578721507d64c4f6e870496d91438
Александр Апарин <aparinalx@yandex.ru>
2023-12-05 14:20:39 +0100

    refact

--- component/ChangeLang.js
+++ component/ChangeLang.js
@@ -1,45 +1,66 @@
-import React from 'react';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
-function ChangeLang({ props }) {
-    const { prefix, inView, setLang, lang } = props;
-    return (
-        <div className={`${!inView ? styles.sticky : ''} ${styles.langMenu}`} style={{ "zIndex": "100" }}>
-            {
-                <a href={`${prefix}/Aparin_CV_${lang}.pdf`} target="_blank" rel="noreferrer" className={styles.downloadCvIcon}>
-                    <Image src={`${prefix}/pdfsymbol.svg`} alt="ru" width={30} height={15} />
-                </a>
-            }
+function ChangeLang({ prefix, inView, setLang, lang }) {
+	return (
+		<div
+			className={`${!inView ? styles.sticky : ""} ${styles.langMenu}`}
+			style={{ zIndex: "100" }}
+		>
+			{
+				<a
+					href={`${prefix}/Aparin_CV_${lang}.pdf`}
+					target="_blank"
+					rel="noreferrer"
+					className={styles.downloadCvIcon}
+				>
+					<Image
+						src={`${prefix}/pdfsymbol.svg`}
+						alt="ru"
+						width={30}
+						height={15}
+					/>
+				</a>
+			}
 
-            <input
-                type="radio"
-                name="langRadio"
-                className={styles.buttonLang}
-                onChange={(e) => setLang(e.target.value)}
-                value="EN"
-                id="buttonEN"
-                checked={lang === "EN"}
-            />
-            <label htmlFor='buttonEN' className={styles.labelButtonLang}>
-                <Image src={`${prefix}/enflag.svg`} alt="en" width={30} height={15} />
-            </label>
+			<input
+				type="radio"
+				name="langRadio"
+				className={styles.buttonLang}
+				onChange={(e) => setLang(e.target.value)}
+				value="EN"
+				id="buttonEN"
+				checked={lang === "EN"}
+			/>
+			<label htmlFor="buttonEN" className={styles.labelButtonLang}>
+				<Image
+					src={`${prefix}/enflag.svg`}
+					alt="en"
+					width={30}
+					height={15}
+				/>
+			</label>
 
-            <input
-                type="radio"
-                name="langRadio"
-                className={`${styles.buttonLang} ${styles.ru}`}
-                onChange={(e) => setLang(e.target.value)}
-                value="RU"
-                id="buttonRU"
-                checked={lang === "RU"}
-            />
-            <label htmlFor='buttonRU' className={styles.labelButtonLang}>
-                <Image src={`${prefix}/ruflag.svg`} alt="ru" width={30} height={15} />
-            </label>
-
-        </div>
-    );
+			<input
+				type="radio"
+				name="langRadio"
+				className={`${styles.buttonLang} ${styles.ru}`}
+				onChange={(e) => setLang(e.target.value)}
+				value="RU"
+				id="buttonRU"
+				checked={lang === "RU"}
+			/>
+			<label htmlFor="buttonRU" className={styles.labelButtonLang}>
+				<Image
+					src={`${prefix}/ruflag.svg`}
+					alt="ru"
+					width={30}
+					height={15}
+				/>
+			</label>
+		</div>
+	);
 }
 
 export default React.memo(ChangeLang);
