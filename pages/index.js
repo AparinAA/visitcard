7272901cffee0c49f81c59e73a437e3cfd3d41e8
Александр Апарин <aparinalx@yandex.ru>
2024-11-07 15:03:15 +0100

    add metrik

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import ChangeLang from "../component/ChangeLang";
 import { readData } from "../data/read";
 import MagicField from "../component/MagicField";
 import GridCards from "../component/GridCards";
+import Script from "next/script";
 
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
@@ -24,7 +25,7 @@ export const metadata = {
 	openGraph: {
 		title: "Aparin Aleksandr",
 		description: "The CV page",
-		url: "https://aparinaleksandr.site",
+		url: "https://aparinaleksandr.me",
 		siteName: "Visitcard",
 		locale: "en_US",
 		type: "website",
@@ -56,7 +57,30 @@ function Home({ data }) {
 				<meta name="description" content="" />
 				<link rel="icon" href={`${prefix}/aa.ico`} />
 			</Head>
+			<Script id="metrika" nonce="XUENAJFW123">
+				{`
+					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();
+					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+					ym(98850439, "init", {
+							clickmap:true,
+							trackLinks:true,
+							accurateTrackBounce:true
+					});
+					`}
+			</Script>
+			<noscript>
+				<div>
+					<img
+						src="https://mc.yandex.ru/watch/98850439"
+						style={{ position: "absolute", left: "-9999px" }}
+						alt=""
+					/>
+				</div>
+			</noscript>
 			<div
 				className={styles.title}
 				id="title"
